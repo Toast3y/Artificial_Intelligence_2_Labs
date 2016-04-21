de7865faec386d9f0850a27e71a5fc2b0d5a6d5e
Toast3y <toast3y@gmail.com>
2016-04-21 13:53:50 +0100

    Added Assignment 2

---
+++ Assignment 2/C12449618+C12474932.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 11 20:11:02 2016
+
+@author: Christopher Jerrard-Dunne
+@student_number: C12449618
+
+@author2: Marcus Daly
+@student_number2: C12474932
+"""
+
+import sys
+import pandas as pd
+
+def main():
+    
+    if(len(sys.argv != 3)):
+        print("Usage: python filename [training_set] [queries]")
+    
+    print("Hello World")
+    
