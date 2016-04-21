16a4843420fd9461bcff01ed4b95460a2cbb4bc4
Toast3y <toast3y@gmail.com>
2016-04-21 15:42:26 +0100

    Added file loading.

--- Assignment 2/C12449618+C12474932.py
+++ Assignment 2/C12449618+C12474932.py
@@ -10,14 +10,38 @@ Created on Mon Apr 11 20:11:02 2016
 """
 
 import sys
-#import pandas as pd
+import pandas as pd
 
 def main():
     
+    trainingSet_dir = ""
+    queries_dir = ""
+    idnum = [0]
+    target = [17]
+    cont = [1, 5, 6, 10, 12, 13, 14, 15]
+    cat = [2, 3, 4, 7, 8, 9, 11, 16]
+    
+    #Find the datasets in the Data folder.
     if len(sys.argv) != 3:
         print("Usage: python filename [training_set] [queries]")
+    else:
+        trainingSet_dir = "./data/" + sys.argv[1]
+        queries_dir = "./data/" + sys.argv[2]
+        
     
-    print("Hello World")
+    #Attempt to open the training and query datasets
+    try:
+        trainingSet = pd.read_csv(trainingSet_dir)
+    except:
+        print("ERROR: Cannot find data file " + trainingSet_dir + " in Data folder. Program will now close.")
+        sys.exit()
+        
+    try:
+        queries = pd.read_csv(queries_dir)
+    except:
+        print("ERROR: Cannot find data file " + queries_dir + " in Data folder. Program will now close.")
+        sys.exit()
+    
     
 
 if __name__ == '__main__':
